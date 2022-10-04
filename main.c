fdd099714903a11970cdbe3395eb4502f92e70b0
usoobin <112929005+usoobin@users.noreply.github.com>
2022-10-04 17:02:20 +0900

    03

--- main.c
+++ main.c
@@ -5,39 +5,21 @@
 
 int main(int argc, char *argv[]) 
 {
+	char c;
+	int cnt =0;
 	
-	int a,b;
-	char oper;
-	int num;
-
-	printf("enter the calculation :");
-	scanf("%i %c %i",&a,&oper,&b);	
+	printf("input a string:");
 	
-	if(oper == '+')
-	{
-		printf("=%i\n",a+b);
-	}
-
-	else if(oper == '-')
-	{
-		printf("=%i\n",a-b);
-	}
-
-	else if(oper == '*')
-	{
-		printf("=%i\n",a*b);
-	}
-
-	else if(oper == '/')
-	{
-		printf("=%i\n",a/b);
-	}
-
-	else if(oper == '%')
-	{
-		printf("=%i\n",a%b);
-	}
+	while((c=getchar())!= '\n') //입력 문자가 개형문자가 나올때까지 반복  
+	{  //만약 입력된 숫자인가? 참-> 그렇다면 count가 한개 증가 
+		if(c >= '0' && c <='9' )
+	 	{
+	 		cnt = cnt + 1;
+	 	}
+		//이 구간이 반복 
+	};
 	
+	printf("the number of digit is %i",cnt);
 	
 	return 0;
 }
