1be6a93c4e0e9bb38f9f1f2f0149031f1a007c7b
usoobin <112929005+usoobin@users.noreply.github.com>
2022-10-04 16:32:59 +0900

    02

--- main.c
+++ main.c
@@ -7,25 +7,21 @@ int main(int argc, char *argv[])
 {
 	
 	int num;
-
-	printf("정수 하나를 입력하시요.");
+	printf("정수 하나를 입력하시오:");
 	scanf("%d",&num);
-
-
-	if(num > 0)
+	
+	
+	if(num >= 0)
 	{
-		printf("양수입니다.");
+		printf("절대값은 %d입니다",num);
+	}
+	
+	else 
+	{
+		printf("절대값은 %d입니다",num*-1);
 	}
 
-	else if(num < 0)
-	{
-		printf("음수입니다.");
-	} 
 
-	else
-	{
-		printf("0입니다");
-	 } 
 	
 	
 	
